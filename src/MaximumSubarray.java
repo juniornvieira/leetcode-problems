09936d10ed8b408764df0f8ea9e055e00f7cf59c
Junior <junior.jaava@gmail.com>
2022-02-19 18:34:05 +0100

    MaximumSubarray - done

---
+++ src/MaximumSubarray.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+        int [] nums = {-2,1,-3,4,-1,2,1,-5,4};
+//        int [] nums = {1};
+//        int [] nums = {5,4,-1,7,8};
+
+        System.out.println(new MaximumSubarray().maxSubArray(nums));
+    }
+
+    public int maxSubArray(int[] nums) {
+        int cur = nums[0];
+        int total = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (cur < 0) {
+                cur = nums[i];
+            } else {
+                cur += nums[i];
+            }
+            if (cur > total) {
+                total = cur;
+            }
+        }
+        return total;
+    }
+}
