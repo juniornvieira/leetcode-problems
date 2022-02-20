11376d03bd7c39fe6ad6fffc4da096fcab468652
Junior <junior.jaava@gmail.com>
2022-02-20 16:35:01 +0100

    LengthOfLastWord - done

--- src/LengthOfLastWord.java
+++ src/LengthOfLastWord.java
@@ -1,2 +1,15 @@
 public class LengthOfLastWord {
+
+    public static void main(String[] args) {
+//        String s = "Hello World";
+//        String s = "   fly me   to   the moon  ";
+        String s = "luffy is still joyboy";
+        System.out.println(new LengthOfLastWord().lengthOfLastWord(s));
+    }
+
+
+    public int lengthOfLastWord(String s) {
+        String[] sArray = s.split(" ");
+        return sArray[sArray.length-1].length();
+    }
 }
